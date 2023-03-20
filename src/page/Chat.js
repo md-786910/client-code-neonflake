9af725d6376f5829e93a5cf066215ce81316164d
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-03-20 10:07:21 +0530

    chat design

--- src/page/Chat.js
+++ src/page/Chat.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Container, Spinner } from 'react-bootstrap'
 import { API } from "./api"
-import { css } from '@emotion/css'
-import ScrollToBottom from 'react-scroll-to-bottom';
 import axios from 'axios';
 
 
@@ -10,7 +8,9 @@ import axios from 'axios';
 function Chat() {
     const [loader, setLoader] = useState(false)
     const [para, setPara] = useState("")
+    const divRef = useRef();
     const [chatResp, setChatResp] = useState(["Hello! How can I assist you today?"])
+
     const [ques, setQues] = useState(["Hi"]);
     const chatBtn = async () => {
         setLoader(true);
@@ -33,60 +33,50 @@ function Chat() {
         }
     }
 
-    const ROOT_CSS = css({
-        height: 500,
-        width: 400
-    });
+
+    useEffect(() => {
+        var objDiv = document.getElementById("your_div");
+        objDiv.scrollTop = objDiv.scrollHeight + 50;
+    }, [])
+
     return (
         <Container fluid className="p-0 mt-5">
-            <div className="spacer">
-                <h1 className='my-4'>
-                    Chat
-                </h1>
-
+            <div className="spacer1">
                 <div >
-                    <div className="chatBox">
-                        <div className="leftChat">
-                            <ScrollToBottom className={ROOT_CSS} mode={'bottom'} checkInterval={150}>
-                                {
-                                    chatResp && chatResp.map((res, index) => {
-                                        return (
-                                            <div className="leftChatBox" key={index}>
-                                                <code><p dangerouslySetInnerHTML={{ __html: res }} /></code>
-                                                <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDyjzMOo0F0BAhDNmTS0mGWPjENA69z6FBgg&usqp=CAU"} width="40" alt="assis" />
-
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </ScrollToBottom>
-                            <div className="loader mt-3">
-                                {
-                                    loader && <Spinner size={30} variant='info' animation="border" role="status">
-                                        <span className="visually-hidden">Loading...</span>
-                                    </Spinner>
-                                }
-                            </div>
-                        </div>
-                        <div className="rightChat">
-                            {
-                                ques && ques.map((que, index) => {
-                                    return (
-                                        <div className="rightChatBox">
-                                            <p key={index}>{que}</p>
+                    <div className="chatBox" id="your_div">
+                        {
+                            ques && ques.map((que, index) => {
+                                return (
+                                    <div className="mb-3" key={index} id="your_div">
+                                        <div className="rightChatBox ">
+                                            <p key={index}>user : {que}</p>
                                         </div>
-                                    )
-                                })
-                            }
+                                        <div className="leftChatBox d-flex align-items-center gap-3" >
+                                            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDyjzMOo0F0BAhDNmTS0mGWPjENA69z6FBgg&usqp=CAU"} width="40" alt="assis" />
+                                            <code><p dangerouslySetInnerHTML={{ __html: chatResp[index] }} /></code>
 
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+
+
+                        <div className="loader mt-3">
+                            {
+                                loader && <Spinner size={30} variant='info' animation="border" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </Spinner>
+                            }
                         </div>
+
 
                     </div>
-                    <div className="mt-3">
-                        <div class="mb-3">
+                    <div className="">
+                        <div>
                             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="john doe" name="para" value={para} onChange={(e) => setPara(e.target.value)} />
                         </div>
-                        <button className='btn btn-outline-dark' onClick={() => chatBtn()}>submit</button>
+                        <button className='mt-1 btn btn-outline-dark' onClick={() => chatBtn()}>submit</button>
 
 
                     </div>
